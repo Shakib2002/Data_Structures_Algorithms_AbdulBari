793dceaae38a84c4b8a775d19840b5b8d900736f
Shakib <shakib123.org@gmail.com>
2024-06-03 19:13:37 +0600

    ARRAY

--- Array/inseart_sort.c
+++ Array/inseart_sort.c
@@ -15,9 +15,24 @@ void Display(struct Array arr)
     for(i=0; i<arr.length; i++)
     printf("%d ",arr.A[i]);
 }
+
+void InsertSort(struct Array *arr, int x)
+{
+    int i=arr->length-1;
+    if(arr->length==arr->size)
+    return;
+while(i>=0 && arr->A[i]>x)
+{
+    arr->A[i+1]=arr->A[i];
+    i--;
+}
+arr->A[i+1]=x;
+arr->length++;
+}
  int main()
  {
      struct Array arr={{2,3,4,5,6,8,4,6,1,9},10,5};
+     InsertSort(&arr,20);
      Display(arr);
   return 0;
  }
